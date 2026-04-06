77aee7a91af2cb77a375f2c17cb0e5e24bc68146
Jude231 <jodamaminiaina492@gmail.com>
2026-04-06 18:27:38 +0300

    Update exo3.py
    Removed commented code and ensured file permissions are set.

--- Gestion du disque dûr et des ﬁchiers/exo3.py
+++ Gestion du disque dûr et des ﬁchiers/exo3.py
@@ -2,7 +2,5 @@ import os
 
 tab = [int(x) for x in input("Entrez les entiers séparés par un espace : ").split()]
 with open("data.bin", "wb") as f:
-    f.write(bytearray(tab)) # Python gère différemment les types d'octets
-    
-# Réglage des permissions : 0o664
+    f.write(bytearray(tab)) 
 os.chmod("data.bin", 0o664)
