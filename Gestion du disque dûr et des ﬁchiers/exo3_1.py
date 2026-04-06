8ca143fdd49f7498f9432f76488da63680e95b72
Jude231 <jodamaminiaina492@gmail.com>
2026-04-06 18:29:49 +0300

    Update exo3_1.py

--- Gestion du disque dûr et des ﬁchiers/exo3_1.py
+++ Gestion du disque dûr et des ﬁchiers/exo3_1.py
@@ -2,10 +2,9 @@ import os
 import struct
 
 taille_fichier = os.path.getsize("data.bin")
-nb_elements = taille_fichier // 4 # Un int fait généralement 4 octets
+nb_elements = taille_fichier // 4 
 
 with open("data.bin", "rb") as f:
-    # On lit tout et on convertit le binaire en entiers
     donnees = f.read()
     tab = list(struct.unpack(f'{nb_elements}i', donnees))
 
